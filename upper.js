3a351e92f7ceb22b1dcbfb7d123f2b686c53fe43
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-13 12:50:53 -0500

    Second attempt works well

--- upper.js
+++ upper.js
@@ -1,3 +1,7 @@
 var practiceString = "Today is Friday, the day before Valentine's Day!"
 practiceString = practiceString.toUpperCase();
 console.log(practiceString);
+
+var practiceString1 = "tODAY IS fRIDAY, THE DAY BEFORE vALENTINE'S dAY!"
+practiceString1 = practiceString1.toLowerCase();
+console.log(practiceString1);
